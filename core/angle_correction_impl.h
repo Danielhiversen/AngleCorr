bd888333ab8dc3fdfd7a27c84fa3825e1cfa8635
Daniel <Daniel.H.Iversen@ntnu.no>
2015-06-23 13:45:43 +0200

    Adding header file

---
+++ core/angle_correction_impl.h
@@ -0,0 +1,12 @@
+#ifndef ANGLE_CORRECTION_IMPL_H
+#define ANGLE_CORRECTION_IMPL_H
+
+#include "spline3d.hpp"
+
+
+typedef vector<Spline3D<double> > vectorSpline3dDouble;
+
+vectorSpline3dDouble*   angle_correction_impl(vtkPolyData *vpd_centerline, const  char* image_prefix , double Vnyq, double cutoff,  int nConvolutions);
+vtkSmartPointer<vtkPolyData> flowDirection( vector<Spline3D<double> > *splines, double uncertainty_limit, double minArrowDist);
+
+#endif /* ANGLE_CORRECTION_IMPL_H */
